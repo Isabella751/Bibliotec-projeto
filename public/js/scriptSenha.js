3b51b53bf25714a0aecea42954f06044951ce9b4
Isabella751 <isabella.leite562@gmail.com>
2025-11-26 11:18:37 -0300

    Tela home com footer e mais livros

--- public/js/scriptSenha.js
+++ public/js/scriptSenha.js
@@ -32,9 +32,9 @@ document.addEventListener("DOMContentLoaded", () => {
         timerSpan.textContent = ` (${tempo}s)`;
     }
 
-    // Quando clicar em “Solicitar”
+
     form.addEventListener("submit", (e) => {
-        e.preventDefault(); // impede redirecionamento imediato
+        e.preventDefault(); 
 
         const email = document.getElementById("email").value.trim();
         if (email === "") {
@@ -42,8 +42,6 @@ document.addEventListener("DOMContentLoaded", () => {
             return;
         }
 
-        // Aqui seria o envio real do email
-        // Mostra feedback
         document.getElementById("enviar").textContent = "Enviando...";
         document.getElementById("enviar").disabled = true;
 
@@ -51,19 +49,13 @@ document.addEventListener("DOMContentLoaded", () => {
             document.getElementById("enviar").textContent = "Solicitar";
             document.getElementById("enviar").disabled = false;
 
-            // Agora exibe a área de reenvio
             reenviarContainer.style.display = "block";
 
-            // Inicia o timer só agora
             iniciarTimer();
 
-            // Redireciona à página de inserir código (opcional)
-            // window.location.href = "redSenha.html";
-
-        }, 1200); // tempo simulado de envio
+        }, 1200); 
     });
 
-    // Quando clicar em reenviar
     link.addEventListener("click", () => {
         link.textContent = "Código reenviado!";
         link.classList.add("desativado");
