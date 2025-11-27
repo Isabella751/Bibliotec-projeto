233ba6b33ff7299397de0e3ecfbcf3618e283809
Giov4nnaxx <gigisantanasilva@gmail.com>
2025-11-27 10:35:33 -0300

    Juntando back com o fronttt

--- projeto_backend/server.js
+++ projeto_backend/server.js
@@ -1,54 +1,42 @@
-// ============================
-//  Dependências
-// ============================
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-
-import path from 'path';  
+import path from 'path';
 import { fileURLToPath } from 'url';
 
-import livrosRoutes from "./routes/livros.routes.js"
-import avaliacoesRoutes from "./routes/avaliacoes.routes.js"
-import usuariosRoutes from "./routes/usuarios.routes.js"
-import favoritosRoutes from "./routes/favoritos.routes.js"
-import reservasRoutes from "./routes/reservas.routes.js"
+import livrosRoutes from "./routes/livros.routes.js";
+import avaliacoesRoutes from "./routes/avaliacoes.routes.js";
+import usuariosRoutes from "./routes/usuarios.routes.js";
+import favoritosRoutes from "./routes/favoritos.routes.js";
+import reservasRoutes from "./routes/reservas.routes.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
-// ============================
-//  Configuração do servidor
-// ============================
 const app = express();
+
+// Middlewares
 app.use(cors());
 app.use(bodyParser.json());
-
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'frontEnd')));
+// Servir arquivos estáticos
+app.use(express.static('public'));
 
-// Servir CSS (se estiver fora do frontEnd)
-app.use('/css', express.static(path.join(__dirname, 'css')));
+// Rotas da API
+app.use("/livros", livrosRoutes);
+app.use("/avaliacoes", avaliacoesRoutes);
+app.use("/usuarios", usuariosRoutes);
+app.use("/favoritos", favoritosRoutes);
+app.use("/reservas", reservasRoutes);
 
-// Servir imagens
-app.use('/images', express.static(path.join(__dirname, 'public/images')));
-app.use('/book_images', express.static(path.join(__dirname, 'public/book_images')));
-
-app.use("/livros", livrosRoutes)
-app.use("/avaliacoes", avaliacoesRoutes)
-app.use("/usuarios", usuariosRoutes)
-app.use("/favoritos", favoritosRoutes)
-app.use("/reservas", reservasRoutes)
-
+// Rota principal
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.resolve('public', 'index.html'));
 });
 
-
-// ============================
-//  Inicia o servidor
-// ============================
+// Iniciar servidor
 const PORT = 3000;
-app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor rodando em http://localhost:3000`);
+});
